1e1d770ecc75beefd387ec1233e93cf84d2e98c0
pelierze <pelierze2@gmail.com>
2025-12-31 09:31:27 +0900

    4week 5day

--- training_center/4week/5day_1_5.py
+++ training_center/4week/5day_1_5.py
@@ -8,3 +8,23 @@ df = pd.DataFrame(data)
 # 문제 5: 'Category' 열을 기준으로 그룹화하여 각 그룹의
 # 첫 번째 행을 출력하세요.
 """
+
+import sys
+import pandas as pd
+
+def solve(data : str) -> str :
+    
+    data = {'Category': ['A', 'B', 'A', 'C', 'B', 'A', 'C', 'C', 'B'],
+        'Value': [10, 20, 15, 25, 18, 12, 22, 28, 19],
+        'Group': [1, 2, 1, 2, 1, 2, 1, 2, 1]}
+    
+    df = pd.DataFrame(data)
+    result = df.groupby("Category").first()
+
+    return result.to_string()
+
+def main() :
+    data = sys.stdin.read().strip()
+
+if __name__ == "__main__" :
+    main()
--- training_center/4week/5day_1_6.py
+++ training_center/4week/5day_1_6.py
@@ -8,3 +8,22 @@ df = pd.DataFrame(data)
 # 문제 6:  'Group' 열을 기준으로 그룹화 한 뒤, 각 그룹에서
 # 'Value' 열의 값이 20 이상인 데이터만 추출하세요.
 """
+import sys
+import pandas as pd
+
+def solve(data : str) -> str :
+    data = {'Category': ['A', 'B', 'A', 'C', 'B', 'A', 'C', 'C', 'B'],
+        'Value': [10, 20, 15, 25, 18, 12, 22, 28, 19],
+        'Group': [1, 2, 1, 2, 1, 2, 1, 2, 1]}
+    df = pd.DataFrame(data)
+
+    result = df[df["Value"] >= 20]
+    
+
+    return result.to_string
+
+def main() :
+    data = sys.stdin().read.strip()
+
+if __name__ == "__main__" :
+    main()
