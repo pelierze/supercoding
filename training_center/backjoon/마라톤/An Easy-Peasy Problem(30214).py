a6ea987fd1ce6b1480ea9529c380970f535f381d
pelierze <pelierze2@gmail.com>
2026-01-02 17:55:31 +0900

    백준 마라톤

---
+++ training_center/backjoon/마라톤/An Easy-Peasy Problem(30214).py
@@ -0,0 +1,50 @@
+"""
+Travis was recently saddled with writing the easy problem for a competitive programming contest aimed at high school students. 
+
+Writing an easy problem is a bit of a challenge for Travis; 
+a problem labeled as “Medium Difficulty Level” by Travis typically ends up being the hardest problem of the set!
+
+But this time things were different. When the contest was over, Travis was happy that everyone solved his problem. 
+
+However, some of the other judges informed Travis a problem is easy only if at least half of the contestants solve the problem in the first half of the contest.
+
+Travis has some data on his problem, and now he wants to determine: was his problem truly easy?
+
+In programming competitions, the term “solves” is used to refer to the number of correct submissions for a problem, i.e., that many contestants solved the problem.
+
+Given the number of solves for Travis’s problem at the end of the first half and at the end of the contest, 
+determine whether at least half the people solved the problem in the first half. Note that solves at the end of the contest includes solves of the first half as well.
+
+입력
+There is only one input line; it contains two integers, s1 and s2 (0 ≤ s1 ≤ s2; 1 ≤ s2 ≤ 100), 
+representing solves of Travis’s problem at the end of the first half and at the end of the contest, respectively.
+
+출력
+Print “E” (quotes for clarity) if the problem is considered easy by the other judges’ standards. 
+Print “H” (quotes for clarity) if the problem is not considered easy by the other judges’ standards. 
+Everyone knows that if Travis is not writing an easy problem, the problem is probably hard!
+"""
+import sys
+
+
+def solve(data : str) -> str :
+    s1, s2 = map(int, data.split())
+
+    std = s2 / 2
+
+    if s1 >= std :
+        result = "E"
+    
+    else :
+        result = "H"
+ 
+        
+    return result
+
+
+def main() :
+    data = sys.stdin.read().strip()
+    print(solve(data))
+
+if __name__ == "__main__" :
+    main()
---
+++ training_center/backjoon/마라톤/줄번호(4470).py
@@ -0,0 +1,27 @@
+"""
+문제
+텍스트에서 줄을 입력받은 뒤, 줄 번호를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 줄의 수 N이 주어진다. 둘째 줄부터 N개의 줄에 각 줄의 내용이 주어진다. 각 줄에 있는 글자의 개수는 50글자를 넘지 않는다.
+
+출력
+각 문장의 앞에 줄 번호를 추가한 뒤 출력한다. 줄 번호는 1번부터 시작한다. 줄번호를 추가하는 형식은 출력 예제를 참고하면 된다.
+"""
+import sys
+
+def solve(data : str) -> str :
+    data_lines = data.splitlines()
+    N = int(data_lines[0])
+    result = []
+    for i in range (1, N + 1) :
+        result.append(f"{i}. {data_lines[i]}")
+
+    return "\n".join(result)
+
+def main() :
+    data = sys.stdin.read().strip()
+    print(solve(data))
+
+if __name__ == "__main__" :
+    main()
